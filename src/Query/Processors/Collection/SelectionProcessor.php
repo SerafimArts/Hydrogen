169c16f070063d323706e93f0211a94b4b5bcc17
Serafim <nesk@xakep.ru>
2017-12-12 23:12:52 +0300

    Add missing classes (ouch)

---
+++ src/Query/Processors/Collection/SelectionProcessor.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of Hydrogen package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Serafim\Hydrogen\Query\Processors\Collection;
+
+use Serafim\Hydrogen\Collection;
+use Serafim\Hydrogen\Query\Criterion\Criterion;
+use Serafim\Hydrogen\Query\Criterion\Select;
+
+/**
+ * Class SelectionProcessor
+ */
+class SelectionProcessor extends CriterionProcessor
+{
+    /**
+     * @param Criterion|Select $criterion
+     * @param Collection $collection
+     * @return Collection
+     * @throws \LogicException
+     */
+    public function process(Criterion $criterion, $collection): Collection
+    {
+        throw new \LogicException(__METHOD__ . ' not implemented yet');
+    }
+}
